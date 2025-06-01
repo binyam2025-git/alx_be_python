f8437fe5daac50c0cc332383f49354430cd2d045
binyam <binyamwork@gmail.com>
2025-06-01 10:53:05 +0300

    feat: Add pattern drawing script

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,7 +1,16 @@
-rows = int(input("Enter the number of rows:"))
-columns = int(input("Enter the number of columns:"))
-symbol = input("Enter a symbol to use:)
-for x in range (rows):
-    for y in range (columns):
-        print (symbol, end= "")
-    print()
+>>> rows = int(input("Enter the number of rows:"))
+Enter the number of rows:4
+>>> columns = int(input("Enter the nymber of columns:"))
+Enter the nymber of columns:4
+>>> symbol = input("Enter a symbol to use:")
+Enter a symbol to use:*
+>>> for x in range (rows):
+...     for y in range(columns):
+...         print (symbol, end= "")
+...     print()
+...
+****
+****
+****
+****
+>>>
