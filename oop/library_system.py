be8f89c54a1c970a2b8c60d5f9c1e41541019422
binyam <binyamwork@gmail.com>
2025-06-20 01:17:52 +0300

    Add library class library_system.py

--- oop/library_system.py
+++ oop/library_system.py
@@ -5,8 +5,11 @@ class Book:
         self.title = title
         self.author = author
 
-    def get_info(self):
+    def __str__(self):
         return f"Book: {self.title} by {self.author}"
+
+    def get_info(self):
+        return str(self)
 
 
 class EBook(Book):
@@ -14,7 +17,7 @@ class EBook(Book):
         super().__init__(title, author)
         self.file_size = file_size  # in KB
 
-    def get_info(self):
+    def __str__(self):
         return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
 
 
@@ -23,7 +26,7 @@ class PrintBook(Book):
         super().__init__(title, author)
         self.page_count = page_count
 
-    def get_info(self):
+    def __str__(self):
         return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
 
 
@@ -39,4 +42,4 @@ class Library:
 
     def list_books(self):
         for book in self.books:
-            print(book.get_info())
+            print(book)
